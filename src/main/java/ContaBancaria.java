763b6d7b04c17e4b61d39b6f7520245977eed2b4
afranio dantas <afraniodantas0224@gmail.com>
2025-05-01 09:45:59 -0300

    feat: add service conta-bancaria

--- src/main/java/ContaBancaria.java
+++ src/main/java/ContaBancaria.java
@@ -1,6 +1,8 @@
 public class ContaBancaria {
     private String titular;
     private double saldo;
+    private double taxaDeposito = 0.005;
+    private double taxaSaque = 0.002;
 
     public ContaBancaria (String titular, double saldo) {
         this.titular = titular;
@@ -16,10 +18,11 @@ public class ContaBancaria {
     }
 
     public void depositar(double valor) {
-        if(valor < 0) {
-            throw new RuntimeException("Deposite um valor maior que 0!");
+        if(valor <= 0) {
+            throw new IllegalArgumentException("Deposite um valor maior que 0!");
         }
-            saldo += valor;
+            double valorMenosTaxa = valor - (valor * taxaDeposito);
+            saldo += valorMenosTaxa;
     }
 
     public void sacar(double valor) {
@@ -27,9 +30,27 @@ public class ContaBancaria {
             throw new RuntimeException("Retire um valor maior que 0!");
         }
 
-        if(saldo < valor) {
+        if (valor > 3000.00) {
+            throw new RuntimeException("O saque não pode ser superior á R$3000.00");
+        }
+
+        double valorMaisTaxa = valor + (valor * taxaSaque);
+        if(saldo < valorMaisTaxa) {
             throw new RuntimeException("Saldo Insuficiente!");
         }
-        saldo -= valor;
+
+        saldo -= valorMaisTaxa;
+    }
+
+    public void transferir(ContaBancaria destino, double valor) {
+        if(valor <= 0) {
+            throw new IllegalArgumentException("Valor da transferência deve ser maior que 0!");
+        }
+
+        if(this.saldo < valor) {
+            throw new RuntimeException("Saldo Insuficiente!");
+        }
+        this.saldo -= valor;
+        destino.saldo += valor;
     }
 }
